9c705d3bed55f00475fc9fbc5f81aa91f8f71375
Данила Комлев <m1902452@edu.misis.ru>
2021-03-29 21:01:28 +0300

    jhgfd

--- Bodroe_ytro_front/SberApp-frontend/src/pages/Workout.js
+++ Bodroe_ytro_front/SberApp-frontend/src/pages/Workout.js
@@ -76,6 +76,7 @@ const Workout = ({
       iterChanged != -1
     ) {
       const getUserAchieves = async () => {
+        alert("userId")
         alert(userId)
         await ApiQueries.createProgressAchieve(userId, new Date(), true);
       };
@@ -295,6 +296,7 @@ const Workout = ({
                             onClick={() => {
                               if (iter + 1 == workoutExercises.length) {
                                 const getUserAchieves = async () => {
+                                  alert("userId")
                                   alert(userId)
                                   await ApiQueries.createProgressAchieve(
                                     userId,
